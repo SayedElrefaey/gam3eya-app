279142224f3fadf6a8890f267ed806e6ee9b4064
SayedElrefaey <sayedelrefaey@gmail.com>
2026-09-09 14:23:02 +0300

    Track turn availability on associations

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -21,13 +21,7 @@ class ScheduleItem {
   final double amount;
   final bool paid;
 
-  ScheduleItem({
-    required this.id,
-    required this.monthIdx,
-    required this.dueDate,
-    required this.amount,
-    required this.paid,
-  });
+  ScheduleItem({required this.id, required this.monthIdx, required this.dueDate, required this.amount, required this.paid});
 
   factory ScheduleItem.fromJson(Map<String, dynamic> j) => ScheduleItem(
         id: int.parse(j['id'].toString()),
@@ -48,39 +42,25 @@ class Gam3eya {
   final String currency;
   final List<ScheduleItem> schedule;
   final List<int> myTurns;
+  final bool hasTurns;
 
-  Gam3eya({
-    required this.id,
-    required this.sectionId,
-    required this.name,
-    required this.startDate,
-    required this.months,
-    required this.monthlyAmount,
-    required this.currency,
-    required this.schedule,
-    required this.myTurns,
-  });
+  Gam3eya({required this.id, required this.sectionId, required this.name, required this.startDate, required this.months, required this.monthlyAmount, required this.currency, required this.schedule, required this.myTurns, required this.hasTurns});
 
   double get total => schedule.fold(0.0, (a, s) => a + s.amount);
   int get paidCount => schedule.where((s) => s.paid).length;
-  double get paidTotal =>
-      schedule.where((s) => s.paid).fold(0.0, (a, s) => a + s.amount);
+  double get paidTotal => schedule.where((s) => s.paid).fold(0.0, (a, s) => a + s.amount);
 
   factory Gam3eya.fromJson(Map<String, dynamic> j) => Gam3eya(
         id: int.parse(j['id'].toString()),
-        sectionId:
-            j['section_id'] == null ? null : int.parse(j['section_id'].toString()),
+        sectionId: j['section_id'] == null ? null : int.parse(j['section_id'].toString()),
         name: j['name'].toString(),
         startDate: j['start_date'].toString(),
         months: int.parse(j['months'].toString()),
         monthlyAmount: double.parse(j['monthly_amount'].toString()),
         currency: j['currency'].toString(),
-        schedule: (j['schedule'] as List)
-            .map((s) => ScheduleItem.fromJson(s))
-            .toList(),
-        myTurns: (j['my_turns'] as List? ?? const [])
-            .map((v) => int.parse(v.toString()))
-            .toList(),
+        schedule: (j['schedule'] as List).map((s) => ScheduleItem.fromJson(s)).toList(),
+        myTurns: (j['my_turns'] as List? ?? const []).map((v) => int.parse(v.toString())).toList(),
+        hasTurns: j['has_turns'].toString() == '1' || j['hasTurns'] == true,
       );
 }
 
@@ -91,13 +71,7 @@ class Entry {
   final String type; // debit / credit
   final String entryDate;
 
-  Entry({
-    required this.id,
-    required this.note,
-    required this.amount,
-    required this.type,
-    required this.entryDate,
-  });
+  Entry({required this.id, required this.note, required this.amount, required this.type, required this.entryDate});
 
   factory Entry.fromJson(Map<String, dynamic> j) => Entry(
         id: int.parse(j['id'].toString()),
@@ -116,26 +90,16 @@ class Individual {
   final String currency;
   final List<Entry> entries;
 
-  Individual({
-    required this.id,
-    required this.sectionId,
-    required this.name,
-    required this.phone,
-    required this.currency,
-    required this.entries,
-  });
+  Individual({required this.id, required this.sectionId, required this.name, required this.phone, required this.currency, required this.entries});
 
-  double get total => entries.fold(
-      0.0, (a, e) => a + (e.type == 'debit' ? e.amount : -e.amount));
+  double get total => entries.fold(0.0, (a, e) => a + (e.type == 'debit' ? e.amount : -e.amount));
 
   factory Individual.fromJson(Map<String, dynamic> j) => Individual(
         id: int.parse(j['id'].toString()),
-        sectionId:
-            j['section_id'] == null ? null : int.parse(j['section_id'].toString()),
+        sectionId: j['section_id'] == null ? null : int.parse(j['section_id'].toString()),
         name: j['name'].toString(),
         phone: (j['phone'] ?? '').toString(),
         currency: j['currency'].toString(),
-        entries:
-            (j['entries'] as List).map((e) => Entry.fromJson(e)).toList(),
+        entries: (j['entries'] as List).map((e) => Entry.fromJson(e)).toList(),
       );
 }
